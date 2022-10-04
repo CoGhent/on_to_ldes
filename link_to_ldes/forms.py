d1d9385c8199ab1a890ec8aa147ba755b076d9a3
Flore <78723853+floreverk@users.noreply.github.com>
2022-10-04 15:41:29 +0200

    Update forms.py

--- link_to_ldes/forms.py
+++ link_to_ldes/forms.py
@@ -3,5 +3,5 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    category = forms.ChoiceField(choices=[('hva', 'Huis van Alijn'), ('dmg', 'Design Museum Gent'), ('industriemuseum', 'Industriemuseum'), ('stam', 'STAM Gent'), ('archief', 'Archief Gent')])
+    institution = forms.ChoiceField(choices=[('hva', 'Huis van Alijn'), ('dmg', 'Design Museum Gent'), ('industriemuseum', 'Industriemuseum'), ('stam', 'STAM Gent'), ('archief', 'Archief Gent')])
     objectnumber = forms.CharField(required=True)
